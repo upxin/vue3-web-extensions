64f28f3cc623f18ff43ac2cfc1d8d78086d6dc21
upxin <upxin1126@163.com>
2025-07-09 13:12:05 +0800

    f

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -204,12 +204,13 @@ function scrollToBottom() {
   <!-- 滚动按钮容器（固定在右侧中间） -->
   <div class="fixed right-4 top-1/2 -translate-y-1/2 flex flex-col gap-2 z-50">
     <Icon
-      icon="cuida:caret-down-outline"
+      icon="cuida:caret-up-outline"
       class="text-[#246999] cursor-pointer transition-transform text-30px"
       @click="scrollToTop"
     />
+
     <Icon
-      icon="cuida:caret-up-outline"
+      icon="cuida:caret-down-outline"
       class="text-[#246999] cursor-pointer transition-transform text-30px"
       @click="scrollToBottom"
     />
